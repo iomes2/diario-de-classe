7a1217f7e1010d405de9977d549349fa7424fa5b
iomes2 <renaniomes10@gmail.com>
2024-11-26 13:33:46 -0300

    versao final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,6 +9,7 @@ import Cadastro from './components/Cadastro/Cadastro';
 import { AuthProvider } from './context/AuthContext'; // Importando o AuthProvider
 import Home from './components/Home/Home';
 import PrivateRoute from './components/PrivateRoute'; // Importando o PrivateRoute
+import RealizarChamada from '.components/R'
 
 const App = () => {
   return (
@@ -22,8 +24,9 @@ const App = () => {
               <PrivateRoute>
                 <Home />
               </PrivateRoute>
-            } 
+            }         
           />
+          <Route path="/RealizarChamada" element={<RealizarChamada />} />
           {/* Adicione outras rotas aqui */}
         </Routes>
       </Router>
